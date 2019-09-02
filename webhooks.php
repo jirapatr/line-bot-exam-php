93d138f2a0e3cbd8cc3cd62a26eda7309c3bb278
jirapatr <oxy258@gmail.com>
2019-09-02 20:49:14 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,8 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'cPjVE/3xK9eqqVRb7gM91IbXLpRgryKvtNHhxM8ozAXTGf3whYkO+PO4B16Al3pdAuCaPH0cIHLz95vyRTVeufUeCnpSGjmZAclHAnlVLszAfBaGzt/H1B7b6iVjp7ZSW5uKYy89gl0DGao7tMdBHAdB04t89/1O/w1cDnyilFU=
+';
 
 // Get POST body content
 $content = file_get_contents('php://input');
