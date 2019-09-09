6745d3e01ed5e602f6cef52d2ded5d94c8ed2ffe
jirapatr <oxy258@gmail.com>
2019-09-09 20:06:52 +0700

    Update line.php

--- line.php
+++ line.php
@@ -6,7 +6,7 @@
         function Auth() {
             var URL = 'https://notify-bot.line.me/oauth/authorize?';
             URL += 'response_type=code';
-            URL += '&client_id=traMRmk4upTzLy909Mxxxx';
+            URL += '&client_id=Be6gfRmqgAE9xt2NUbNgf9';
             URL += '&redirect_uri=http://localhost/test/index.php';//ถ้า login แล้ว เลือกกลุ่มหรือตัวเอง ให้กลับมาหน้านี้
             URL += '&scope=notify';
             URL += '&state=oxy258@gmail.com';//กำหนด  user หรือ อะไรก็ได้ที่สามารถบอกถึงว่าเป็น user ในระบบ
