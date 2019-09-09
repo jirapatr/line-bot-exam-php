0565c378ea59d8ad033ee3d88883adc6b561e15c
jirapatr <oxy258@gmail.com>
2019-09-09 20:08:52 +0700

    Update line.php

--- line.php
+++ line.php
@@ -7,7 +7,7 @@
             var URL = 'https://notify-bot.line.me/oauth/authorize?';
             URL += 'response_type=code';
             URL += '&client_id=Be6gfRmqgAE9xt2NUbNgf9';
-            URL += '&redirect_uri=http://localhost/test/index.php';//ถ้า login แล้ว เลือกกลุ่มหรือตัวเอง ให้กลับมาหน้านี้
+            URL += '&redirect_uri=http://dukeace.herokuapp.com/line.php';//ถ้า login แล้ว เลือกกลุ่มหรือตัวเอง ให้กลับมาหน้านี้
             URL += '&scope=notify';
             URL += '&state=oxy258@gmail.com';//กำหนด  user หรือ อะไรก็ได้ที่สามารถบอกถึงว่าเป็น user ในระบบ
             window.location.href = URL;
